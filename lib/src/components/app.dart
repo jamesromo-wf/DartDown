ba42b48c6a37a430327e54c9ecde38d87b214e87
James Romo <james.romo@workiva.com>
2017-06-27 17:02:26 -0700

    Additional improvements for text input and searching

--- lib/src/components/app.dart
+++ lib/src/components/app.dart
@@ -28,8 +28,7 @@ class AppComponent extends UiStatefulComponent<AppProps, AppState>{
 
   @override
   render(){
-    return (Dom.div())(
-        Dom.h1()("Dart Down"),
+    return GridFrame()(
         (Block()
           ..className = 'rootNodeDemoClasses'
         )(
@@ -62,6 +61,67 @@ class AppComponent extends UiStatefulComponent<AppProps, AppState>{
                 ..isNested = false
               )(
                 (TextInput()..size = InputSize.LARGE)(),
+                // Not currently working
+                (FilterableDropdownMenuPrimitive()
+                  ..defaultSearchQuery = 'Alien'
+                  ..searchFilter = (dynamic item, String searchQuery) {
+                    // For this implementation, we know all the items are going to be SelectOptions...
+                    var selectOptionProps = SelectOption()
+                      ..addAll(new SelectOptionComponent().getDefaultProps())
+                      ..addAll(getProps(item));
+
+                    return selectOptionProps.displayText.toUpperCase().contains(searchQuery.toUpperCase());
+                  }
+                )(
+                  (SelectOption()
+                    ..displayText = 'The Adventures of Buckaroo Banzai Across the 8th Dimension'
+                    ..value = 1
+                  )('The Adventures of Buckaroo Banzai Across the 8th Dimension'),
+                  (SelectOption()
+                    ..displayText = 'Alien'
+                    ..value = 2
+                  )('Alien'),
+                  (SelectOption()
+                    ..displayText = 'Alien Nation'
+                    ..value = 3
+                  )('Alien Nation'),
+                  (SelectOption()
+                    ..displayText = 'Aliens'
+                    ..value = 4
+                  )('Aliens'),
+                  (SelectOption()
+                    ..displayText = 'Buck Rogers in the 25th Century'
+                    ..value = 5
+                  )('Buck Rogers in the 25th Century'),
+                  (SelectOption()
+                    ..displayText = 'Enemy Mine'
+                    ..value = 6
+                  )('Enemy Mine'),
+                  (SelectOption()
+                    ..displayText = 'Event Horizon'
+                    ..value = 7
+                  )('Event Horizon'),
+                  (SelectOption()
+                    ..displayText = 'Flash Gordon'
+                    ..value = 8
+                  )('Flash Gordon'),
+                  (SelectOption()
+                    ..displayText = 'Galaxy Quest'
+                    ..value = 9
+                  )('Galaxy Quest'),
+                  (SelectOption()
+                    ..displayText = 'Spaceballs'
+                    ..value = 10
+                  )('Spaceballs'),
+                  (SelectOption()
+                    ..displayText = 'Star Wars'
+                    ..value = 11
+                  )('Star Wars'),
+                  (SelectOption()
+                    ..displayText = 'Star Trek'
+                    ..value = 12
+                  )('Star Trek')
+                ),
                 ListGroup()(
                   ListGroupItem()('Test1'),
                   ListGroupItem()('Test2'),
@@ -85,7 +145,14 @@ class AppComponent extends UiStatefulComponent<AppProps, AppState>{
             ),
             (BlockContent()
               ..className = 'content5'
-            )('Preview'),
+            )(
+              (AutosizeTextarea()
+                ..label = 'Basic Example'
+                ..hideLabel = true
+                ..placeholder = 'New note...'
+                ..className = 'noteArea'
+              )()
+            ),
         ),
     );
   }
